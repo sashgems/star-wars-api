7b9e29dfffeaf460ae8d1df22010c78fc2f9f4ad
Sasha Gemis <sashgems@Sashas-MacBook-Air.local>
2019-12-07 00:12:50 -0500

    the get method by filter now works as well as the post

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express')
 const models = require('./models')
 
+const bodyParser = require('body-parser')
+//We need this so we can destructure rebel attributes for parsing
 let app = express()
+
+app.use(bodyParser.json())
 
 app.get('/rebels', (request, response) => {
   models.Rebels.findAll().then((rebels) => {
     response.send(rebels)
   })
 })
-
-app.get('/rebels/:identifier',(request, response) => {
-  if (typeof request.params.identifier === 'number') {
-    models.Rebels.findAll({ where: { id: request.params.identifier }, }).then((rebel) => {
+//To search by a filter either id or callSign
+app.get('/rebels/:filter',(request, response) => {
+  if (!isNaN(request.params.filter)) {
+    models.Rebels.findOne({ where: { id: request.params.filter }, }).then((rebel) => {
       response.send(rebel)
     })
   } else {
-    models.Rebels.findAll({ where: { callSign: request.params.identifier }, }).then((rebel) => {
+    models.Rebels.findOne({ where: { callSign: request.params.filter }, }).then((rebel) => {
       response.send(rebel)
+
     })
+    
   }
 })
 
 app.post('/rebels', (request, response) => {
   const { name, callSign, rank } = request.body
-
-  if (!name || !callSign || !rank) {
+ 
+  if (!name || !callSign|| !rank) {
     response.status(400).send('The following attributes are required: name, callSign, rank')
   }else
   models.Rebels.create({ name, callSign, rank }).then((newRebel) => {
